c27c2f9b3eee62a61a495fd448ba3e87fa973b75
sakshamunme00 <97934681+sakshamunme00@users.noreply.github.com>
2023-11-13 09:58:20 +0530

    jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,7 +11,7 @@ const api = process.env.API_URL;
         isRevoked: isRevoked
 }).unless({
     path: [
-        {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+       
         {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
         {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
         {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST','PUT']},

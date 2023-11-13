33d3d469f9cb31f3d903954f57b6f6e15e1245c9
sakshamunme00 <97934681+sakshamunme00@users.noreply.github.com>
2023-11-13 09:35:26 +0530

    products.js

--- routes/products.js
+++ routes/products.js
@@ -93,35 +93,6 @@ router.get(`/get/featured/:count`, async (req, res) =>{
     } 
     res.send(products);
 })
-router.put(
-    
-    async (req, res)=> {
-        if(!mongoose.isValidObjectId(req.params.id)) {
-            return res.status(400).send('Invalid Product Id')
-         }
-         const files = req.files
-         let imagesPaths = [];
-         const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
 
-         if(files) {
-            files.map(file =>{
-                imagesPaths.push(`${basePath}${file.filename}`);
-            })
-         }
-
-         const product = await Product.findByIdAndUpdate(
-            req.params.id,
-            {
-                images: imagesPaths
-            },
-            { new: true}
-        )
-
-        if(!product)
-            return res.status(500).send('the gallery cannot be updated!')
-
-        res.send(product);
-    }
-)
 
 module.exports =router;
